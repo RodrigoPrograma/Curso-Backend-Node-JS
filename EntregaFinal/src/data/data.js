809cb6c4790b6b7ec0c1bf63ebeaf0f29a55a19a
Rodrigo Alonzo <118147090+RodrigoPrograma@users.noreply.github.com>
2025-12-10 22:47:59 -0300

    actualizada config de firebase

--- EntregaFinal/src/data/data.js
+++ EntregaFinal/src/data/data.js
@@ -14,4 +14,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
 export { db };
